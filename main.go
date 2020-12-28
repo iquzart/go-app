a9bc6566c3cb1fd9fc86d8e3adda7c73abf9a267
iquzart <iquzart@hotmail.com>
2020-12-28 11:46:19 +0400

    remove port

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"os"
-
 	"github.com/iquzart/go-app/routers"
 )
 
 func main() {
 
-	port := ":" + os.Getenv("PORT")
+	//port := ":" + os.Getenv("PORT")
 
 	routersInit := routers.InitRouter()
 
-	if port == ":" {
-		routersInit.Run()
-	} else {
-		routersInit.Run(port)
-	}
+	//if port == ":" {
+	routersInit.Run()
+	//} else {
+	//	routersInit.Run(port)
+	//}
 }
